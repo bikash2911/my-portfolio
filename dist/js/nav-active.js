1eecd02ee1e27275c35da9205ee90dc91a34a0ae
Kindness <kindness@Kindnesss-MacBook-Air.local>
2026-02-24 03:06:04 +0300

    navbar active

--- dist/js/nav-active.js
+++ dist/js/nav-active.js
@@ -1,6 +1,8 @@
 const links = document.querySelectorAll(".navbar-link");
-const currentPage =
-  location.pathname.split("/").filter(Boolean).pop() || "index.html";
+
+// pathname ko trailing slash hataune ra empty lai index.html set garne
+let currentPage = location.pathname.replace(/\/$/, ""); // trailing slash remove
+currentPage = currentPage.split("/").pop() || "index.html";
 
 links.forEach((link) => {
   const linkPage = link.getAttribute("href");
